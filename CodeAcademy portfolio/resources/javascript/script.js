358f074fefeff8dd55481671ec8b19d39189a0f3
chrisagonza97 <75388370+chrisagonza97@users.noreply.github.com>
2021-11-26 17:14:17 -0800

    code academy personal site

---
+++ CodeAcademy portfolio/resources/javascript/script.js
@@ -0,0 +1,49 @@
+
+function handleTeaClick(){
+    if(document.getElementById("tea-fig").style.display!='block'){
+        document.getElementById("tea-fig").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("tea-fig").style.display = "none";
+    }
+}
+
+function handleDasmotosClick(){
+    if(document.getElementById("dasmotos-fig").style.display!='block'){
+        document.getElementById("dasmotos-fig").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("dasmotos-fig").style.display = "none";
+    }
+}
+
+function handleCalcClick(){
+    if(document.getElementById("calc-fig").style.display!='block'){
+        document.getElementById("calc-fig").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("calc-fig").style.display = "none";
+    }
+}
+
+function handleDesignClick(){
+    if(document.getElementById("design-fig").style.display!='block'){
+        document.getElementById("design-fig").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("design-fig").style.display = "none";
+    }
+}
+function handleQuoteClick(){
+    if(document.getElementById("quote-fig").style.display!='block'){
+        document.getElementById("quote-fig").style.display = "block";
+        
+    }
+    else{
+        document.getElementById("quote-fig").style.display = "none";
+    }
+}
